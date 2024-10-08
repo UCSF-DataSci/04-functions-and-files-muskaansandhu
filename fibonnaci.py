b60cf509638f0d7280248aba098c75aafe1405a6
MuskaanSandhu <156035268+muskaansandhu@users.noreply.github.com>
2024-10-08 13:33:29 -0700

    exercise 1 output final

--- fibonnaci.py
+++ fibonnaci.py
@@ -13,18 +13,37 @@ Create a program that generates Fibonacci numbers less than a limit and writes t
 
 Task: Generate the Fibonacci numbers less than 100 and write them to `fibonacci_100.txt`
 """
+import argparse
 
+#DEFINING FIBONACCI SEQUENCE FUNCTION
 def fibonacci_sequence(limit):
 	fib_numbers = [] #generating empty list to store the Fibonacci numbers 
 	first_num = 0 #initializing first number of sequence 
 	second_num = 1 #initializing second number of sequence 
 	while first_num < limit: #making sure that the numbers are less than upper limit 
 		fib_numbers.append(first_num) #adding Fibonacci numbers to the list 
-		sum = first_num + second_num #adding Fibonacci numbers together 
+		fib_sums = first_num + second_num #adding Fibonacci numbers together 
 		first_num = second_num #moving to next Fibonacci number 
-		second_num = sum #moving to next Fibonacci number 
+		second_num = fib_sums #moving to next Fibonacci number 
 	return fib_numbers #returning list 
+
+#USING ARGPARSE TO SPECIFY UPPER LIMIT AND OUTPUT FILE NAME FOR COMMAND LINE ARGUMENTS 
+parser = argparse.ArgumentParser()
+parser.add_argument("limit", type=int, help="Upper limit for Fibonacci sequence") #defining limit 
+parser.add_argument("filename", type=str, help="Output file name") #defining output file name 
 
 
 if __name__ == "__main__":
-    # def some_function()
+    args = parser.parse_args()  
+    result = fibonacci_sequence(args.limit)  
+    try:
+        with open(args.filename, 'w') as file:  
+            for number in result:  
+                file.write(f"{number}\n")  
+        print(f"Fibonacci numbers written to {args.filename}") 
+    except IOError as error:
+        print(f"Error writing to file: {error}")  
+
+
+
+
